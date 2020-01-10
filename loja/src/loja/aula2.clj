c52e1d4b31770b0c9b275f3003f8a174b4154814
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-01-09 23:39:54 -0300

    O que aprendemos nesta aula:
    Como o reduce funciona;
    Implementar o reduce;
    Variação de parâmetros na função;
    Utilizar o loop;
    Fazer recorrência dentro do loop.

--- loja/src/loja/aula2.clj
+++ loja/src/loja/aula2.clj
@@ -66,3 +66,23 @@
 
 
 (println (conta 0 ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"]))
+
+
+; FOR total-ate-agora 0 , elementos-restantes elementos   ;; 1 next
+(defn conta
+  [elementos]
+  (println "antes do loop")
+  (loop [total-ate-agora 0
+         elementos-restantes elementos]
+    (if (seq elementos-restantes)
+      (recur (inc total-ate-agora) (next elementos-restantes))
+      total-ate-agora)))
+
+
+(println (conta ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"]))
+
+(println (conta []))
+
+
+
+
