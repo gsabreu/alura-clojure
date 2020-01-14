d93f5b3dd3c205b23c4251d5939c3c662325655b
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-01-13 23:03:12 -0300

    O que aprendemos nesta aula:
    Simular um banco em memória;
    Utilizar o require para fazer a importação de classe;
    Utilizar um as para abreviação;
    Agrupar dados.

--- loja/src/loja/aula3.clj
+++ loja/src/loja/aula3.clj
@@ -58,7 +58,6 @@
   [[_ detalhes]]
   (* (get detalhes :quantidade 0) (get detalhes :preco-unitario 0)))
 
-
 (defn total-do-pedido
   [pedido]
   (->> pedido
@@ -72,15 +71,14 @@
        (map total-do-pedido)
        (reduce +)))
 
-(defn quantia-de-pedidos-e-gastos-total-por-usuario
+(defn quantia-de-pedidos-e-gasto-total-por-usuario
   [[usuario pedidos]]
-  {:usuario-id usuario
-   :total-de-pedidos(count pedidos)
-   :preco-total (total-dos-pedidos pedidos)})
-
+  {:usuario-id       usuario
+   :total-de-pedidos (count pedidos)
+   :preco-total      (total-dos-pedidos pedidos)})
 
 
 (->> (l.db/todos-os-pedidos)
      (group-by :usuario)
-     (map quantia-de-pedidos-e-gastos-total-por-usuario)
+     (map quantia-de-pedidos-e-gasto-total-por-usuario)
      println)
